a3b19c9be386475fad85b885e49d3f013456bf6f
cronusqiu90 <cronusqiu90@gmail.com>
2024-06-24 10:25:10 +0800

    update amqp log

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # golib
+
+Common go packages
--- amqp/logger.go
+++ amqp/logger.go
@@ -11,31 +11,31 @@ import (
 // WithPublisherOptionsLogger() or WithConsumerOptionsLogger().
 type Logger logger.Logger
 
-const loggingPrefix = "gorabbit"
+const loggingPrefix = "[amqp]"
 
 type stdDebugLogger struct{}
 
 // Fatalf -
 func (l stdDebugLogger) Fatalf(format string, v ...interface{}) {
-	log.Fatalf(fmt.Sprintf("%s FATAL: %s", loggingPrefix, format), v...)
+	log.Fatalf(fmt.Sprintf("%s: %s", loggingPrefix, format), v...)
 }
 
 // Errorf -
 func (l stdDebugLogger) Errorf(format string, v ...interface{}) {
-	log.Printf(fmt.Sprintf("%s ERROR: %s", loggingPrefix, format), v...)
+	log.Printf(fmt.Sprintf("[E] %s: %s", loggingPrefix, format), v...)
 }
 
 // Warnf -
 func (l stdDebugLogger) Warnf(format string, v ...interface{}) {
-	log.Printf(fmt.Sprintf("%s WARN: %s", loggingPrefix, format), v...)
+	log.Printf(fmt.Sprintf("[W] %s: %s", loggingPrefix, format), v...)
 }
 
 // Infof -
 func (l stdDebugLogger) Infof(format string, v ...interface{}) {
-	log.Printf(fmt.Sprintf("%s INFO: %s", loggingPrefix, format), v...)
+	log.Printf(fmt.Sprintf("[I] %s: %s", loggingPrefix, format), v...)
 }
 
 // Debugf -
 func (l stdDebugLogger) Debugf(format string, v ...interface{}) {
-	log.Printf(fmt.Sprintf("%s DEBUG: %s", loggingPrefix, format), v...)
+	log.Printf(fmt.Sprintf("[D] %s: %s", loggingPrefix, format), v...)
 }
